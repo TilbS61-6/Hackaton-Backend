94556e53a7b028ea5710bafb1cf0e8206d4f6807
teun de clercq <t.declercq@student.fontys.nl>
2021-03-01 14:27:09 +0100

    test

---
+++ .github/actions/readDir.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const dot = "."
+
+
+try {
+    let newArray = [];
+
+    fs.readdir("./", (err, files)=> {
+        if (err) {
+            console.log(err.message);
+        } else {
+            newArray = files.filter((file, index, object) => {
+                return !file.includes(dot) && !file.includes("_");
+            });
+            newArray.filter((file) => {
+                isDirEmpty(file).then(log => {
+                    console.log(log);
+                })
+                console.log(file + " " + fs.statSync(file).isDirectory());
+            });
+        }
+    });
+
+    function isDirEmpty(dirname) {
+        return fs.promises.readdir(dirname).then(files => {
+            return files.length > 0;
+        })
+    }
+    core.setOutput("directorys", newArray);
+} catch (e) {
+    core.setFailed(e.message);
+}
+
+
+
